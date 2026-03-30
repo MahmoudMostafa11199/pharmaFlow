b552a3671f0dec6cfba39f14f4a5c607a001152c
Mahmoud Mostafa Elshahat <m_devolepment@hotmail.com>
2026-03-30 10:35:30 +0200

    config

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  next();
+});
+
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
