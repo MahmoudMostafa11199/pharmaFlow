50594ee6007661864732c673dc953832ebe2a736
Mahmoud Mostafa Elshahat <m_devolepment@hotmail.com>
2026-03-31 21:01:40 +0200

    fix: use absolute path for db.json with __dirname

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+const path = require('path');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('server/db.json');
+const dbPath = path.join(__dirname, 'server', 'db.json');
+const router = jsonServer.router(dbPath);
 const middlewares = jsonServer.defaults();
+
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  next();
+});
 
 server.use(middlewares);
 server.use(router);
